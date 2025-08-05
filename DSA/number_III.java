195a1f35a4b4e9e1e48edfb0dae66c5fa049ae8f
AdityaBhadre <adityabhadre18@gmail.com>
2025-08-05 21:47:40 +0530

    Day 11: Practiced problem on number series

--- DSA/number_III.java
+++ DSA/number_III.java
@@ -55,7 +55,50 @@ class number_III {
             System.out.print(num+" is not an Abundant Number");
         }
     }
+    static void findRoots(int a, int b, int c){
+        if( a == 0 ){
+            System.out.print("Roots are Invalid");
+            return ;
+        }
+        int d = b * b - (4 * a * c);
+        double sqrt_val = Math.sqrt(Math.abs(d));
+        if(d > 0){
+            System.out.println("Roots are real and different");
+            double root1 = (double)(- b + sqrt_val) / (2 * a);
+            double root2 = (double)(- b - sqrt_val) / (2 * a);
+            System.out.print(root1+ "\n"+root2);
+        }else if(d == 0){
+            System.out.println("Roots are real and equal");
+            double root1 = (double)(- b) / (2 * a);
+            double root2 = (double)(- b) / (2 * a);
+            System.out.print(root1 +"\n"+root2);
+        }else{
+            System.out.println("Roots are complex");
+            System.out.print((double)(- b) / (2 * a)+ "+ i"+ sqrt_val+"\n"+(double)(- b) / (2 * a)+"- i"+sqrt_val );
+        }
+    }
+    static void replace0By1(int num){
+        int d;
+        int ans = 0;
+        int temp = 1;
+        if(num == 0){
+            System.out.print(1);
+        }
+        while(num > 0){
+            d = num % 10;
+            if(d == 0){
+                d = 1;
+            }
+            ans = d * temp + ans ;
+            num /= 10;
+            temp *= 10;
+        }
+        System.out.print(ans);
+    }   
     public static void main(String[] args) {
+        int a = 1, b = -3, c = -10;
+        findRoots(a,b,c);
+        replace0By1(1002301);
         addFraction(1,5,1,4);
         possibleCombination(6, 4);
         isAbundantNumber(18);
