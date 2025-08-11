6986d705b6bec9563fdfb8053561d072d8730049
AdityaBhadre <adityabhadre18@gmail.com>
2025-08-11 21:59:41 +0530

    Day 14: Practiced Sorting Type

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -1,4 +1,44 @@
+import java.util.*;
 class sorting {
+     private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low;      
+        int right = mid + 1;   
+
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2 ;
+        mergeSort(arr, low, mid);  
+        mergeSort(arr, mid + 1, high); 
+        merge(arr, low, mid, high);  
+    }
     static void quickSort(int arr[], int lb, int ub) {
         if (lb < ub) {
             int loc = partition(arr, lb, ub);
@@ -74,6 +114,7 @@ class sorting {
         int n = arr.length;
         insertionSort(arr);
         quickSort(arr,0,n-1);
+        mergeSort(arr, 0, n - 1);
         for(int i = 0; i < n; i++){
             System.out.print(arr[i]+" ");
         }
